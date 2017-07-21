f810efc365cde8799dedca9c01681468d3c5971e
Waqas84 <walsubayee@yahoo.com>
2017-07-21 12:38:58 -0700

    been deployed

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friendsArray = [
+  {
+    name: "Waqas",
+    photo: "http://lorempixel.com/400/200/sports/1",
+    scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    name: "Messi",
+    photo: "http://images.performgroup.com/di/library/goal_es/72/56/lionel-messi-sevilla-barcelona-laliga-06112016_1d1wu95d62alx1ai4xwymyrxoj.jpg?t=997769880&w=620&h=430",
+    scores: [
+      1,
+      2,
+      4,
+      4,
+      3,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  }
+];
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,42 @@
+var friendsList = require("../data/friends.js");
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsList);
+    });
+
+    app.post("/api/friends", function(req, res) {
+        var userScore = req.body.scores.map(function(score) {
+            return parseInt(score);
+        });
+
+        var scoreArray = [];
+        
+        for (var i = 0; i < friendsList.length; i++) {
+            var scoreDifference = 0;
+           
+            for (var j = 0; j < userScore.length; j++) {
+                
+                scoreDifference += Math.abs(parseInt(friendsList[i].scores[j]) - (userScore[j]));
+            }
+        
+            scoreArray.push(scoreDifference);
+        }
+        var smallestIndex = 0;
+
+       
+        for (var i = 0; i < scoreArray.length; i++) {
+            if (scoreArray[i] <= scoreArray[smallestIndex]) {
+                smallestIndex = i;
+            }
+        }
+
+        var bestMatch = friendsList[smallestIndex];
+        friendsList.push(req.body);
+        res.json(bestMatch);
+         res.redirect("/");
+
+    });
+   
+};
